7a4e8d4cba0791d608fbe1cd399ace6aedd5c38a
andro095 <andre.rodriguezgt@yahoo.com>
2025-04-18 18:33:19 -0400

    Example Updating

--- sampleScripts/pysparkexample.py
+++ sampleScripts/pysparkexample.py
@@ -23,7 +23,6 @@ kafka_df = spark.readStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", "localhost:9092") \
     .option("subscribe", "test-s-5") \
-    .option("checkpointLocation", "file:////home/duty095/chkpt") \
     .option("startingOffsets", "earliest") \
     .load()
 
