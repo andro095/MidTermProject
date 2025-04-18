f800a63f799c5b80d7861c4698cc27f48463da59
andro095 <andre.rodriguezgt@yahoo.com>
2025-04-18 15:49:35 -0400

    Example Updating

--- sampleScripts/pysparkexample.py
+++ sampleScripts/pysparkexample.py
@@ -23,6 +23,7 @@ kafka_df = spark.readStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", "localhost:9092") \
     .option("subscribe", "iot-sink") \
+    .option("checkpointLocation", "file:////home/duty095/chkpt") \
     .option("startingOffsets", "earliest") \
     .load()
 
