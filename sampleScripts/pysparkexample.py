ca4217f005196152cd7fe231c3dfb63841c67a08
andro095 <andre.rodriguezgt@yahoo.com>
2025-04-18 18:29:23 -0400

    Example Updating

--- sampleScripts/pysparkexample.py
+++ sampleScripts/pysparkexample.py
@@ -22,7 +22,7 @@ json_schema = StructType([
 kafka_df = spark.readStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", "localhost:9092") \
-    .option("subscribe", "test-s-4") \
+    .option("subscribe", "test-s-5") \
     .option("checkpointLocation", "file:////home/duty095/chkpt") \
     .option("startingOffsets", "earliest") \
     .load()
