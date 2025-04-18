f8dd671469b332c5c42fff13a4e40f632ba0537e
andro095 <andre.rodriguezgt@yahoo.com>
2025-04-18 15:45:07 -0400

    Example Updating

--- sampleScripts/pysparkexample.py
+++ sampleScripts/pysparkexample.py
@@ -22,7 +22,7 @@ json_schema = StructType([
 kafka_df = spark.readStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", "localhost:9092") \
-    .option("subscribe", "my-news") \
+    .option("subscribe", "iot-sink") \
     .option("startingOffsets", "earliest") \
     .load()
 
