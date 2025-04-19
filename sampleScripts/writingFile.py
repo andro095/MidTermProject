ce03115a8712f3a54a1f3473dfed3391a63562f6
andro095 <andre.rodriguezgt@yahoo.com>
2025-04-19 19:29:58 -0400

    Example Updating

--- sampleScripts/writingFile.py
+++ sampleScripts/writingFile.py
@@ -22,7 +22,7 @@ json_schema = StructType([
 kafka_df = spark.readStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", "localhost:9092") \
-    .option("subscribe", "test-s-6") \
+    .option("subscribe", "test-s-19") \
     .option("startingOffsets", "earliest") \
     .load()
 
