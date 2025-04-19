9946f3f51f972dcfa8ef933d9492f66a347fbe77
andro095 <andre.rodriguezgt@yahoo.com>
2025-04-19 16:28:14 -0400

    Example Updating

--- writingAggregation.py
+++ writingAggregation.py
@@ -1,10 +1,14 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType
 from pyspark.sql.functions import from_json, col
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Initialize Spark Session for Dataproc
 spark = SparkSession.builder \
-    .appName("KafkaStreamingExample") \
+    .appName(os.getenv('SPARK_APP_NAME')) \
     .getOrCreate()
 
 json_schema = (StructType()
@@ -19,8 +23,8 @@ json_schema = (StructType()
 
 raw_stream = spark.readStream \
     .format("kafka") \
-    .option("kafka.bootstrap.servers", "localhost:9092") \
-    .option("subscribe", "test-s-7") \
+    .option("kafka.bootstrap.servers", f"{os.getenv('GCP_LOCAL_HOST')}:{os.getenv('GCP_KAFKA_PORT')}") \
+    .option("subscribe", os.getenv('GCP_KAFKA_TOPIC')) \
     .option("startingOffsets", "earliest") \
     .load()
 
@@ -32,7 +36,7 @@ parsed_stream = raw_stream \
 query = parsed_stream.writeStream \
     .outputMode("append") \
     .format("console") \
-    .option("checkpointLocation", "/BigData/mchkpt") \
+    .option("checkpointLocation", os.getenv('CHKPT_DIR')) \
     .trigger(processingTime='3 seconds') \
     .start()
 
